0470b9ad88a0f9289bf55b31ecb95eff2fc20565
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2024-01-22 21:05:58 +0545

    tailwind2

--- Serveasy/src/App.jsx
+++ Serveasy/src/App.jsx
@@ -3,8 +3,16 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className=" text-blue-700">hello</h1>
-      <h2 className=" bg-slate-700">123</h2>
+      <section className="bg-red-600">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Corporis placeat itaque enim tempora
+          veritatis praesentium temporibus reiciendis neque
+          consequuntur suscipit autem explicabo recusandae
+          consectetur facere, quidem vero! Debitis, sunt
+          pariatur.
+        </p>
+      </section>
     </>
   );
 }
