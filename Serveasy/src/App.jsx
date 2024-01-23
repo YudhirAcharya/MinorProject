af1f0d9547ba85c26500df7a75d0eb8457fc341d
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2024-01-23 15:38:50 +0545

    inserted logo

--- Serveasy/src/App.jsx
+++ Serveasy/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
+      <img src="./public/logos/logo_name.png" alt="" />
+
       <div className=" bg-slate-500 w-full overflow-hidden">
         <h1>Serveasy</h1>
       </div>
--- Serveasy/tailwind.config.js
+++ Serveasy/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primaryColor: "#fcde05",
+    },
     extend: {},
   },
   plugins: [],
