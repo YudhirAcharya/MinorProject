007a9a5c9bba303752286de7ffc2ed378d2cfcc2
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2024-01-26 14:36:47 +0545

    form input log

--- Serveasy/src/components/FormInput.jsx
+++ Serveasy/src/components/FormInput.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const FormInput = (props) => {
   return (
-    <div className="flex flex-col w-[220px]">
+    <div className="flex flex-col w-[270px]">
       <label htmlFor="">{props.title}</label>
-      <input type={props.type} className="border-2" />
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        className="border-2"
+        name={props.name}
+      />
     </div>
   );
 };
--- Serveasy/src/pages/SignUp.jsx
+++ Serveasy/src/pages/SignUp.jsx
@@ -1,22 +1,49 @@
 import React from "react";
 import FormInput from "../components/FormInput";
-
+import { useRef } from "react";
 const Signup = () => {
+  const fullNameRef = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault(); //to prevent refresh on click
+    console.log(fullNameRef);
+    const data = new FormData(event.target);
+    //user data
+    console.log(Object.fromEntries(data.entries()));
+  };
   return (
-    <div className="flex flex-col  w-1/2  border-2 text-center justify-center mx-auto my-auto pb-5">
+    <div className="flex flex-col w-4/5 md:w-1/2 border-2 text-center justify-center mx-auto my-auto pb-5">
       Sign Up
       <form
         action=""
         className=" flex flex-col items-center "
+        onSubmit={handleSubmit}
       >
-        <FormInput title="Full Name" type="text" />
-        <FormInput title="User Name" type="text" />
-        <FormInput title="Email" type="email" />
+        <FormInput
+          title="Full Name"
+          type="text"
+          placeholder="Gregor Clegane"
+          name="fullname"
+        />
+        <FormInput
+          title="User Name"
+          type="text"
+          placeholder="Gregor Gigglesbane"
+          name="username"
+        />
+        <FormInput
+          title="Email"
+          type="email"
+          placeholder="GregorGiggles@gmail.com"
+          name="email"
+        />
         <FormInput title="Password" type="password" />
         <FormInput
           title="Confirm Password"
           type="password"
         />
+        <button className=" border-2 rounded-md m-2 p-2 bg-primaryColor">
+          Submit
+        </button>
       </form>
     </div>
   );
