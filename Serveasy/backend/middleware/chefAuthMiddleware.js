62e5dde9d4eb32d49316ef9b712bfb13fa7ab9f6
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2024-02-12 19:33:58 +0545

    Refactored Chef Routes,Controllers

---
+++ Serveasy/backend/middleware/chefAuthMiddleware.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET_CHEF,
+      (err, decodedToken) => {
+        if (err) {
+          console.log(err);
+          // res.redirect("/chef/login");
+          res.json({ error: "Redirect to chef login page" });
+        } else {
+          console.log(decodedToken);
+          next();
+        }
+      },
+    );
+  } else {
+    // res.redirect("/chef/login");
+    res.json({ error: "Redirect to chef login page" });
+  }
+};
+
+// check current user
+const checkChef = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET_CHEF,
+      (err, decodedToken) => {
+        if (err) {
+          res.locals.user = null;
+          next();
+        } else {
+          const pool = req.pool;
+          pool.getConnection((err, connection) => {
+            if (err) throw err;
+            connection.query(
+              "SELECT * FROM chef WHERE c_email=?",
+              [decodedToken.email],
+              (err, result) => {
+                connection.release();
+                if (err) {
+                  // Handle the error
+                  console.log("Error retrieving chef:", err);
+                  return res
+                    .status(500)
+                    .json({ status: "error", error: "Internal Server Error" });
+                }
+                if (result.length == 1) {
+                  res.locals.user = result[0]; // Assuming the user object is stored in `result[0]`
+                  next();
+                }
+              },
+            );
+          });
+        }
+      },
+    );
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkChef };
